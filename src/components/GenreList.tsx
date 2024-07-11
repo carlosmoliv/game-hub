f03c7307b970d7c0afbc510e2501ce5640b0d85f
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-07-11 15:48:22 -0300

    fetch genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import useGenres from '../hooks/useGenres';
+
+const GenreList = () => {
+  const { genres } = useGenres();
+
+  return (
+    <div>
+      {genres.map((genre) => (
+        <ul key={genre.id}>{genre.name}</ul>
+      ))}
+    </div>
+  );
+};
+
+export default GenreList;
