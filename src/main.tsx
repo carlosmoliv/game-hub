d3002cdb73841d4c63c9578ed03de3c088821f92
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-07-29 16:24:08 -0300

    chore: add react-query dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import theme from './theme'
+import theme from './theme';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
